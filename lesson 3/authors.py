110823a1391fcbcf2f005697f2093356dca0fc3b
Francis Rodrigues <dev@tosystems.net>
2017-09-03 23:33:39 -0300

    Solved quiz 9 of lesson 3: Handling Attributes

--- lesson 3/authors.py
+++ lesson 3/authors.py
@@ -16,12 +16,17 @@ def get_authors(root):
         data = {
             "fnm": None,
             "snm": None,
-            "email": None
+            "email": None,
+            "insr": []
         }
 
         fnm = author.find('./fnm').text
         snm = author.find('./snm').text
         email = author.find('./email').text
+        insr = author.findall('./insr')
+
+        for i in insr:
+            data["insr"].append(i.attrib["iid"])
 
         data["fnm"] = fnm
         data["snm"] = snm
@@ -34,14 +39,14 @@ def get_authors(root):
 
 def test():
     solution = [
-        {'fnm': 'Omer', 'snm': 'Mei-Dan', 'email': 'omer@extremegate.com'},
-        {'fnm': 'Mike', 'snm': 'Carmont', 'email': 'mcarmont@hotmail.com'},
-        {'fnm': 'Lior', 'snm': 'Laver', 'email': 'laver17@gmail.com'},
-        {'fnm': 'Meir', 'snm': 'Nyska', 'email': 'nyska@internet-zahav.net'},
-        {'fnm': 'Hagay', 'snm': 'Kammar', 'email': 'kammarh@gmail.com'},
-        {'fnm': 'Gideon', 'snm': 'Mann', 'email': 'gideon.mann.md@gmail.com'},
-        {'fnm': 'Barnaby', 'snm': 'Clarck', 'email': 'barns.nz@gmail.com'},
-        {'fnm': 'Eugene', 'snm': 'Kots', 'email': 'eukots@gmail.com'}
+        {'insr': ['I1'], 'fnm': 'Omer', 'snm': 'Mei-Dan', 'email': 'omer@extremegate.com'},
+        {'insr': ['I2'], 'fnm': 'Mike', 'snm': 'Carmont', 'email': 'mcarmont@hotmail.com'},
+        {'insr': ['I3', 'I4'], 'fnm': 'Lior', 'snm': 'Laver', 'email': 'laver17@gmail.com'},
+        {'insr': ['I3'], 'fnm': 'Meir', 'snm': 'Nyska', 'email': 'nyska@internet-zahav.net'},
+        {'insr': ['I8'], 'fnm': 'Hagay', 'snm': 'Kammar', 'email': 'kammarh@gmail.com'},
+        {'insr': ['I3', 'I5'], 'fnm': 'Gideon', 'snm': 'Mann', 'email': 'gideon.mann.md@gmail.com'},
+        {'insr': ['I6'], 'fnm': 'Barnaby', 'snm': 'Clarck', 'email': 'barns.nz@gmail.com'},
+        {'insr': ['I7'], 'fnm': 'Eugene', 'snm': 'Kots', 'email': 'eukots@gmail.com'}
     ]
 
     root = get_root(article_file)
@@ -49,6 +54,7 @@ def test():
 
     assert data[0] == solution[0]
     assert data[1]["fnm"] == solution[1]["fnm"]
+    assert data[1]["insr"] == solution[1]["insr"]
 
 
 test()
