8e011169368ed24b146537c44d9512141649d3ff
Francis Rodrigues <dev@tosystems.net>
2017-09-10 18:31:18 -0300

    Lesson 3: 19 - Quiz: Using Beautiful Soup

---
+++ lesson 3/html_soup.py
@@ -0,0 +1,52 @@
+#!~/envs/python3/udacity_python_mongodb
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Please note that the function 'make_request' is provided for your reference only.
+# You will not be able to to actually use it from within the Udacity web UI.
+# Your task is to process the HTML using BeautifulSoup, extract the hidden
+# form field values for "__EVENTVALIDATION" and "__VIEWSTATE" and set the appropriate
+# values in the data dictionary.
+# All your changes should be in the 'extract_data' function
+from bs4 import BeautifulSoup
+import requests
+import json
+
+html_page = "datasets/air_trans_page_source.html"
+
+
+def extract_data(page):
+    data = {"eventvalidation": "",
+            "viewstate": ""}
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        pass
+
+    return data
+
+
+def make_request(data):
+    eventvalidation = data["eventvalidation"]
+    viewstate = data["viewstate"]
+
+    r = requests.post("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+                      data={'AirportList': "BOS",
+                            'CarrierList': "VX",
+                            'Submit': 'Submit',
+                            "__EVENTTARGET": "",
+                            "__EVENTARGUMENT": "",
+                            "__EVENTVALIDATION": eventvalidation,
+                            "__VIEWSTATE": viewstate
+                            })
+
+    return r.text
+
+
+def test():
+    data = extract_data(html_page)
+    assert data["eventvalidation"] != ""
+    assert data["eventvalidation"].startswith("/wEWjAkCoIj1ng0")
+    assert data["viewstate"].startswith("/wEPDwUKLTI")
+
+
+test()
