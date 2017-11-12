882ff4e888db84511a5173d7c552f40d3f5a6582
Francis Rodrigues <dev@tosystems.net>
2017-11-12 16:31:43 -0200

    Solved quiz 10 of lesson 7: Finding Porsche

--- lesson 7/find_porsche.py
+++ lesson 7/find_porsche.py
@@ -14,9 +14,50 @@ https://www.udacity.com/wiki/ud032
 """
 
 
+def add_car(db):
+    db.autos.insert([
+        {
+            "layout": "NULL",
+            "name": "Ford GT40",
+            "productionYears": [],
+            "modelYears": [],
+            "bodyStyle": [
+                "coupé",
+                "roadster"
+            ],
+            "assembly": "NULL",
+            "class": [
+                "group 4 (racing)",
+                "group 6 (racing)"
+            ],
+            "manufacturer": [
+                "Carrol Shelby International",
+                "Ford _Advanced_Vehicles",
+                "John Wyer",
+                "Kar_Kraft"
+            ]
+        },
+        {
+            "layout": "rear mid-engine rear-wheel-drive layout",
+            "name": "Porsche Boxster",
+            "productionYears": [],
+            "modelYears": [],
+            "bodyStyle": "roadster",
+            "assembly": [
+                "Finland",
+                "Germany",
+                "Stuttgart",
+                "Uusikaupunki"
+            ],
+            "class": "sports car",
+            "manufacturer": "Porsche"
+        }
+    ])
+
+
 def porsche_query():
     # Please fill in the query to find all autos manufactured by Porsche.
-    query = {}
+    query = {"manufacturer": "Porsche"}
     return query
 
 
@@ -33,6 +74,7 @@ def find_porsche(db, query):
 
 if __name__ == "__main__":
     db = get_db('examples')
+    # add_car(db)
     query = porsche_query()
     results = find_porsche(db, query)
 
