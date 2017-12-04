9b3400bddaf02dc2c50fbfe39d7d55720728e1f0
Francis Rodrigues <dev@tosystems.net>
2017-12-04 17:41:49 -0200

    Solved quiz 25 of lesson 7: Dot Notation

--- lesson 7/dot_find.py
+++ lesson 7/dot_find.py
@@ -16,7 +16,7 @@ def dot_query():
     # Edit the line below with your query - try to use dot notation.
     # You can check out 'example_auto.txt' for an example of the document
     # structure in the collection.
-    query = {}
+    query = {"dimensions.width": {"$gt": 2.5}}
     return query
 
 
