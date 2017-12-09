34e0548df0c13d26a8a6c3efdf6f445d00b078b1
Francis Rodrigues <dev@tosystems.net>
2017-12-09 12:53:55 -0200

    Lesson 8: 2. Quiz: Inserting into DB

---
+++ lesson 8/dbinsert.py
@@ -0,0 +1,27 @@
+#!~/envs/udacity_python3_mongodb
+
+"""
+Complete the insert_data function to insert the data into MongoDB.
+"""
+
+import json
+
+DATA_DIR = 'datasets/'
+DATAFILE = DATA_DIR + 'arachnid.json'
+
+
+def insert_data(data, db):
+    # Your code here. Insert the data into a collection 'arachnid'
+    pass
+
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.examples
+
+    with open(DATAFILE) as f:
+        data = json.loads(f.read())
+        insert_data(data, db)
+        print(db.arachnid.find_one())
